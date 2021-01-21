6f964877b171f6f2bc63851b507b41149b6e338c
yama7mi3 <yama7mi3@gmail.com>
2021-01-21 09:54:34 +0900

    Update file

--- pages/index.js
+++ pages/index.js
@@ -30,31 +30,13 @@ function Home() {
   return (
     <main className={styles.main}>
       <h1>Fast Refresh Demo</h1>
-      <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
-      </p>
-      <hr className={styles.hr} />
       <div>
-        <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
-        </p>
-        <p>Current value: {count}</p>
+        <p>現在値: {count}</p>
       </div>
-      <hr className={styles.hr} />
       <div>
-        <p>Component with state.</p>
         <ClickCount />
       </div>
-      <hr className={styles.hr} />
       <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
         <Button
           onClick={(e) => {
             setTimeout(() => document.parentNode(), 0)
@@ -64,7 +46,6 @@ function Home() {
           Throw an Error
         </Button>
       </div>
-      <hr className={styles.hr} />
     </main>
   )
 }
