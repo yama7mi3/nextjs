d599e250d6c4125068d4c8832c39ca2b2cde811d
naomi.yamashita <naomi.yamashita@jointcrew.co.jp>
2021-01-21 09:37:55 +0900

    Update file

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,72 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useCallback, useEffect, useState } from 'react'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Button from '../components/Button'
+import ClickCount from '../components/ClickCount'
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+function throwError() {
+  console.log(
+    // The function body() is not defined
+    document.body()
   )
 }
+
+function Home() {
+  const [count, setCount] = useState(0)
+  const increment = useCallback(() => {
+    setCount((v) => v + 1)
+  }, [setCount])
+
+  useEffect(() => {
+    const r = setInterval(() => {
+      increment()
+    }, 1000)
+
+    return () => {
+      clearInterval(r)
+    }
+  }, [increment])
+
+  return (
+    <main className={styles.main}>
+      <h1>Fast Refresh Demo</h1>
+      <p>
+        Fast Refresh is a Next.js feature that gives you instantaneous feedback
+        on edits made to your React components, without ever losing component
+        state.
+      </p>
+      <hr className={styles.hr} />
+      <div>
+        <p>
+          Auto incrementing value. The counter won't reset after edits or if
+          there are errors.
+        </p>
+        <p>Current value: {count}</p>
+      </div>
+      <hr className={styles.hr} />
+      <div>
+        <p>Component with state.</p>
+        <ClickCount />
+      </div>
+      <hr className={styles.hr} />
+      <div>
+        <p>
+          The button below will throw 2 errors. You'll see the error overlay to
+          let you know about the errors but it won't break the page or reset
+          your state.
+        </p>
+        <Button
+          onClick={(e) => {
+            setTimeout(() => document.parentNode(), 0)
+            throwError()
+          }}
+        >
+          Throw an Error
+        </Button>
+      </div>
+      <hr className={styles.hr} />
+    </main>
+  )
+}
+
+export default Home
